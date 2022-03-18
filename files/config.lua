f0c2da9a405d34ad1a0cde66df31ee277246c5a6
tmorgan-nagronia <58902626+tmorgan-nagronia@users.noreply.github.com>
2022-03-18 11:29:10 +0000

    2022-03-18 TMorgan - Updated lua config.
    Now includes spamhaus feeds as as well as local.

--- files/config.lua
+++ files/config.lua
@@ -1,1 +1,9 @@
+
+-- Local Example File 
 rpzFile("/etc/pdns-recursor/local.rpz", {})
+
+-- rpzMaster({"34.194.195.25","35.156.219.71"}, "drop.ip.dtq", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
+-- rpzMaster({"34.194.195.25","35.156.219.71"}, "coinblocker.srv", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
+-- rpzMaster({"34.194.195.25","35.156.219.71"}, "doh.dtq", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
+rpzMaster({'34.194.195.25","35.156.219.71'}, 'porn.host.srv', {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
+-- rpzMaster({"34.194.195.25","35.156.219.71"}, "torblock.srv", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
