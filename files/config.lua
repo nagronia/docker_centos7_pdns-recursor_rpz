796b7df59c078d91ea78c8ce12242986130473cb
tmorgan-nagronia <58902626+tmorgan-nagronia@users.noreply.github.com>
2022-03-18 11:52:27 +0000

    2022-03-18 TMorgan - lua script corrections.

--- files/config.lua
+++ files/config.lua
@@ -2,8 +2,18 @@
 -- Local Example File 
 rpzFile("/etc/pdns-recursor/local.rpz", {})
 
--- rpzMaster({"34.194.195.25","35.156.219.71"}, "drop.ip.dtq", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
--- rpzMaster({"34.194.195.25","35.156.219.71"}, "coinblocker.srv", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
--- rpzMaster({"34.194.195.25","35.156.219.71"}, "doh.dtq", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
-rpzMaster({'34.194.195.25","35.156.219.71'}, 'porn.host.srv', {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
--- rpzMaster({"34.194.195.25","35.156.219.71"}, "torblock.srv", {defpol=Policy.Custom, defcontent="badserver.example.com",refresh=900})
+
+-- drop.ip.dtq - the Spamhaus Don't Router or Peer list
+--rpzPrimary({"34.194.195.25","35.156.219.71"}, "drop.ip.dtq", {defpol=Policy.NXDOMAIN,refresh=900})
+
+-- coinblocker.srv - blocks lookups of bitcoin type mining sites
+--rpzPrimary({"34.194.195.25","35.156.219.71"}, "coinblocker.srv", {defpol=Policy.NXDOMAIN,refresh=900})
+
+-- doh.dtq - blocks DNS over HTTPS (DoH) for Firefox
+--rpzPrimary({"34.194.195.25","35.156.219.71"}, "doh.dtq", {defpol=Policy.NXDOMAIN,refresh=900})
+
+-- porn.host.srv - a porn blocking list
+rpzPrimary({"34.194.195.25","35.156.219.71"}, "porn.host.srv", {defpol=Policy.Drop})
+
+-- torblock.srv - blocks access to Tor Exit Nodes
+--rpzPrimary({"34.194.195.25","35.156.219.71"}, "torblock.srv", {defpol=Policy.NXDOMAIN, refresh=900, extendedErrorCode = 15, extendedErrorExtra = "Blocked by policy"})
